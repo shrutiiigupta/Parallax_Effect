327a065eeb65867655686e186491ff34a50711bd
Shruti Gupta <202151151@iiitvadodara.ac.in>
2023-07-30 01:19:53 +0530

    contacts added

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ let leftFront = document.getElementById('leftFront');
 let rightFront = document.getElementById('rightFront');
 let backest = document.getElementById('backest');
 // let rightBack = document.getElementById('rightBack');
-let cloudr = document.getElementById('cloudr');
-let cloudrlow = document.getElementById('cloudrlow');
-let cloudleft = document.getElementById('cloudleft');
-let cloudlow = document.getElementById('cloudlow');
-let cloudm = document.getElementById('cloudm');
-let clouds = document.getElementById('clouds');
 
 window.addEventListener('scroll',() => {
     let value = window.scrollY;
@@ -23,3 +17,23 @@ window.addEventListener('scroll',() => {
 
 })
 
+
+
+function sendEmail(){
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("message").value;
+
+    Email.send({
+        SecureToken : "b3831c65-ee25-4441-8df1-7951acb47a6c",
+        To : '202151151@iiitvadodara.ac.in',
+        From : "shrutigupta.kin@gmail.com",
+        Subject : "Email from Contact Form",
+        Body : "Name : " + name + "<br/> User Email : " + email + 
+        "<br/> Contact Number : " + phone + "<br/> Message : " + message
+    }).then(
+      message => alert("Message sent successfully")
+    );
+}
